a1a235ae5b53ef3efad3a36e03b8f13f4a7a3511
VincenzoY <vincenzoyee@gmail.com>
2020-07-12 21:42:09 +0000

    can delete data and find nodes

--- main.rb
+++ main.rb
@@ -8,6 +8,106 @@ class Node
   end
 end
 
+class Tree
+  attr_accessor :tree
+
+  def initialize(array)
+    @tree = build_tree(array)
+  end
+
+  def insert(value)
+    value = [value]
+    if @tree.data < value[0]
+      current_node = @tree.r_child
+      @tree.r_child = add(value, current_node)
+    elsif root_node.data > value[0]
+      current_node = @tree.l_child
+      @tree.l_child = add(value, current_node)
+    else
+      array.shift
+    end
+    return @tree
+  end
+
+  def delete(value)
+    value = [value]
+    if @tree.data < value[0]
+      current_node = @tree.r_child
+      @tree.r_child = remove(value, current_node)
+    elsif @tree.data > value[0]
+      current_node = @tree.l_child
+      @tree.l_child = remove(value, current_node)
+    else
+      @tree = recombine(@tree.l_child, @tree.r_child)
+    end
+    return @tree
+  end
+
+  def find(value)
+    value = [value]
+    if @tree.data < value[0]
+      current_node = @tree.r_child
+      return locate(value, current_node)
+    elsif @tree.data > value[0]
+      current_node = @tree.l_child
+      return locate(value, current_node)
+    end
+    return @tree
+  end
+
+  private
+
+  def add(array, current_node)
+    if current_node == nil
+      return Node.new(array.shift)
+    elsif current_node.data < array[0]
+      current_node.r_child = add(array, current_node.r_child)
+      return current_node
+    elsif current_node.data > array[0]
+      current_node.l_child = add(array, current_node.l_child)
+      return current_node
+    elsif current_node.data == array[0]
+      array.shift
+      return current_node
+    end
+  end
+
+  def remove(array, current_node)
+    if current_node == nil
+      return
+    elsif current_node.data < array[0]
+      current_node.r_child = remove(array, current_node.r_child)
+      return current_node
+    elsif current_node.data > array[0]
+      current_node.l_child = remove(array, current_node.l_child)
+      return current_node
+    elsif current_node.data == array[0]
+      current_node = recombine(current_node.l_child, current_node.r_child)
+    end
+  end
+
+  def recombine(branch, current_node)
+    if current_node == nil
+      return branch
+    else
+      current_node.l_child = recombine(branch, current_node.l_child)
+      return current_node
+    end
+  end
+
+  def locate(array, current_node)
+    if current_node == nil
+      return
+    elsif current_node.data < array[0]
+      return locate(array, current_node.r_child)
+    elsif current_node.data > array[0]
+      return locate(array, current_node.l_child)
+    elsif current_node.data == array[0]
+      return current_node
+    end
+  end
+end
+
 def build_tree(array)
   root_node = Node.new(array.shift)
   until array.empty?
@@ -24,20 +124,7 @@ def build_tree(array)
   return root_node
 end
 
-def add(array, current_node)
-  if current_node == nil
-    return Node.new(array.shift)
-  elsif current_node.data < array[0]
-    current_node.r_child = add(array, current_node.r_child)
-    return current_node
-  elsif current_node.data > array[0]
-    current_node.l_child = add(array, current_node.l_child)
-    return current_node
-  elsif current_node.data == array[0]
-    array.shift
-    return current_node
-  end
-end
-
-build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 1])
-
+x = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 1])
+x.insert(10000)
+x.delete(8)
+p x.find(4)
